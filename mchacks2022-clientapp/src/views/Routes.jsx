94377382dbd0ab27e8de24f195681228928c21e7
Jordan Gagnon <itsseraphii@gmail.com>
2023-01-28 19:41:49 -0500

    Added events route and page

--- mchacks2022-clientapp/src/views/Routes.jsx
+++ mchacks2022-clientapp/src/views/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import Home from "./home/Home";
 import Welcome from "./welcome/Welcome";
+import Events from "./events/Events";
 
 export default function Routes() {
   return (
@@ -9,6 +10,7 @@ export default function Routes() {
     <Switch>
       <Route exact path='/home' component={Home}/>
       <Route exact path='/welcome' component={Welcome}/>
+      <Route exact path='/events' component={Events}/>
       <Redirect from='/' exact to='/home'/>
       <Redirect to='/not-found'/>
     </Switch>
