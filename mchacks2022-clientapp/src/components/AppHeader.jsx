30d55c6e8412a8118315c855c2b3931b2b36a115
William Fecteau <william-fecteau@hotmail.com>
2023-01-28 22:04:36 -0500

    Completed header

--- mchacks2022-clientapp/src/components/AppHeader.jsx
+++ mchacks2022-clientapp/src/components/AppHeader.jsx
@@ -1,10 +1,12 @@
 import { Icon } from "@iconify/react";
 import LoginModal from "./LoginModal";
-import { useState } from "react";
-import {Link} from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../App";
 
 export default function AppHeader() {
   const [showLoginModal, setShowLoginModal] = useState(false);
+  const { user } = useContext(UserContext);
 
   return (
     <>
@@ -15,13 +17,43 @@ export default function AppHeader() {
         <Link className="hover:cursor-pointer" to="/">
           Commun<span className="text-primary">EAT</span>y
         </Link>
-        <div
-          className="text-navGreen text-base cursor-pointer hover:text-primary"
-          onClick={() => setShowLoginModal(true)}
-        >
-          <Icon icon="material-symbols:login" className="inline mr-1" />
-          Login
-        </div>
+        {user?.username === null && (
+          <div
+            className="text-navGreen text-base cursor-pointer hover:text-primary"
+            onClick={() => setShowLoginModal(true)}
+          >
+            <Icon icon="material-symbols:login" className="inline mr-1" />
+            Login
+          </div>
+        )}
+        {user?.username !== null && (
+          <div className="flex gap-16">
+            <div
+              className="text-navGreen text-base cursor-pointer hover:text-primary"
+              onClick={() => setShowLoginModal(true)}
+            >
+              <Icon
+                icon="material-symbols:event-available-outline-rounded"
+                className="inline mr-1"
+              />
+              Events
+            </div>
+            <div
+              className="text-navGreen text-base cursor-pointer hover:text-primary"
+              onClick={() => setShowLoginModal(true)}
+            >
+              <Icon icon="carbon:checkmark" className="inline mr-1" />
+              Joined
+            </div>
+            <div
+              className="text-navGreen text-base cursor-pointer hover:text-primary"
+              onClick={() => setShowLoginModal(true)}
+            >
+              <Icon icon="bi:people-circle" className="inline mr-1" />
+              Account
+            </div>
+          </div>
+        )}
       </nav>
       <LoginModal show={showLoginModal} setShow={setShowLoginModal} />
     </>
