1be142f2a75b2ef81621a59fd7dea33cd0c6c72d
William Fecteau <william-fecteau@hotmail.com>
2023-01-29 05:43:12 -0500

    Fuck callback hell

---
+++ database/eventDto.js
@@ -0,0 +1,83 @@
+const { dbAll, dbGet } = require("./sqliteExtensions");
+
+async function getMilestonesAsync(bd, eventId) {
+  let query =
+    "SELECT * FROM EventMilestone WHERE fk_eventId = ? ORDER BY minNbUsers";
+  return await dbAll(bd, query, [eventId]);
+}
+
+async function getCurNbUsersAsync(bd, eventId) {
+  let query =
+    "SELECT COUNT(*) AS curNbUsers FROM UsersEvents WHERE fk_eventId = ?";
+  let response = await dbGet(bd, query, [eventId]);
+
+  return response["curNbUsers"];
+}
+
+async function getEventsAsync(bd) {
+  let query = "SELECT * FROM Events";
+  return await dbAll(bd, query);
+}
+
+async function getFullEventAsync(bd, eventId) {
+  let eventQuery = `SELECT * FROM Events INNER JOIN Restaurants R on R.pk_id = Events.fk_restaurantId WHERE Events.pk_id = ?`;
+
+  let event = await dbGet(bd, eventQuery, [eventId]);
+  let milestones = await getMilestonesAsync(bd, eventId);
+  let curNbUsers = await getCurNbUsersAsync(bd, eventId);
+
+  return _mapFullEvent(event, milestones, curNbUsers);
+}
+
+async function getFullEventsAsync(bd) {
+  let eventsQuery = `SELECT Events.pk_id as eventId FROM Events INNER JOIN Restaurants R on R.pk_id = Events.fk_restaurantId`;
+
+  let events = await dbAll(bd, eventsQuery);
+
+  let fullEvents = [];
+  for (let event of events) {
+    let curFullEvent = await getFullEventAsync(bd, event.eventId);
+    fullEvents.push(curFullEvent);
+  }
+
+  return fullEvents;
+}
+
+function _computeCurPrice(event, milestones, curNbUsers) {
+  let curPrice = event.basePrice;
+  if (milestones.length > 0) {
+    var biggestMilestone = milestones[0];
+
+    for (var i = 0; i < milestones.length; i++) {
+      if (curNbUsers < milestones[i].minNbUsers) break;
+
+      biggestMilestone = milestones[i];
+    }
+
+    curPrice -= biggestMilestone.priceDiscount;
+  }
+  return curPrice;
+}
+
+function _mapFullEvent(event, milestones, curNbUsers) {
+  return {
+    imageUrl: event.imageUrl,
+    name: event.restaurantName,
+    date: event.eventDate,
+    food: event.eventName,
+    address: event.restaurantAddress,
+    ogPrice: event.basePrice,
+    curPrice: _computeCurPrice(event, milestones, curNbUsers),
+    curUsers: curNbUsers,
+    maxUsers: event.maxNbUsers,
+    milestones: milestones,
+  };
+}
+
+module.exports = {
+  getMilestonesAsync,
+  getCurNbUsersAsync,
+  getEventsAsync,
+  getFullEventAsync,
+  getFullEventsAsync,
+};
--- mchacks2022-clientapp/src/views/events/Events.jsx
+++ mchacks2022-clientapp/src/views/events/Events.jsx
@@ -1,7 +1,7 @@
-import React, {useContext, useEffect, useState} from "react";
-import {Icon} from "@iconify/react";
+import React, { useContext, useEffect, useState } from "react";
+import { Icon } from "@iconify/react";
 import EventCard from "../../components/EventCard";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Events() {
@@ -12,33 +12,30 @@ function Events() {
     (async () => {
       try {
         const res = await axios.get("/events");
-        console.log(res)
-
+        console.log(res);
+        setEvents(res.data);
       } catch (ex) {
-        console.error(ex)
+        console.error(ex);
       }
     })();
   }, []);
-
 
   return (
     <div className="flex flex-col px-8 my-8">
       <div className="flex mb-8 ml-4">
         <div className="text-3xl mr-8">Nearby Events</div>
-        <button
-          className="flex items-center bg-green-500 hover:bg-green-400 hover:shadow pl-2 pr-4 rounded font-semibold">
-          <Icon icon={"ic:baseline-plus"} width="32px"/>
+        <button className="flex items-center bg-green-500 hover:bg-green-400 hover:shadow pl-2 pr-4 rounded font-semibold">
+          <Icon icon={"ic:baseline-plus"} width="32px" />
           Event
         </button>
       </div>
       <div className="flex flex-wrap max-w-6xl">
-        {events.map(event => (
-          <Link to='/details'>
-            <EventCard event={event}/>
+        {events.map((event) => (
+          <Link to="/details">
+            <EventCard event={event} />
           </Link>
         ))}
       </div>
-
     </div>
   );
 }
