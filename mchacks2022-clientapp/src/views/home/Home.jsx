781ae821cc825073ea1afa303f0f95c87511f309
colehoux <charlo.lehoux@gmail.com>
2023-01-29 11:27:10 -0500

    Mon tour de reformat

--- mchacks2022-clientapp/src/views/home/Home.jsx
+++ mchacks2022-clientapp/src/views/home/Home.jsx
@@ -1,29 +1,29 @@
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import PrimaryButton from "../../components/PrimaryButton";
 import LoginModal from "../../components/LoginModal";
-import { useState } from "react";
-import { UserContext } from "../../App";
-import { useNavigate } from "react-router-dom";
+import {useState} from "react";
+import {UserContext} from "../../App";
+import {useNavigate} from "react-router-dom";
 
 function Home() {
   const [showLoginModal, setShowLoginModal] = useState(false);
-  const { user } = useContext(UserContext);
+  const {user} = useContext(UserContext);
   const navigate = useNavigate();
 
   return (
     <>
-      <div className="flex w-full justify-center">
+      <div className="flex w-full justify-center" style={{marginLeft: "180px"}}>
         <div className="flex flex-col mx-32">
           <div className="flex flex-col flex-wrap mt-8">
             <div className="font-extrabold text-4xl">
               Eat and Save,
-              <br />
+              <br/>
               Together 😎
             </div>
             <div className="mt-8">
-              Save money 💸 <br />
-              Discover new cuisines 🧑‍🍳 <br />
-              Meet new people 🤝 <br />
+              Save money 💸 <br/>
+              Discover new cuisines 🧑‍🍳 <br/>
+              Meet new people 🤝 <br/>
             </div>
           </div>
 
@@ -41,15 +41,16 @@ function Home() {
         </div>
         <img
           className="self-center"
-          style={{ width: "512px" }}
+          style={{width: "512px"}}
           alt="Good food is considered to be nutritious and flavorful, made with high-quality ingredients and prepared in a way that enhances their natural taste. It is often cooked with care and attention to detail, and is visually appealing as well. Additionally, good food should be free of harmful ingredients and should be consumed in moderation as part of a balanced diet."
           src={
             "https://cdn.discordapp.com/attachments/482569552933289984/1069090172131627128/test.png"
           }
         />
       </div>
-      <LoginModal show={showLoginModal} setShow={setShowLoginModal} />
+      <LoginModal show={showLoginModal} setShow={setShowLoginModal}/>
     </>
   );
 }
+
 export default Home;
